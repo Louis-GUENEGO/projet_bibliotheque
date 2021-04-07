5b2bd40b188dda7cdd84b8bfc9055b11f0803c87
CRESCENCE Benjamin <Benjamin.Crescence@bordeaux-inp.fr>
2021-04-07 12:26:15 +0200

    modif livre.h

--- src/livre.h
+++ src/livre.h
@@ -1,12 +1,24 @@
 #include "ressource.h"
 
+#ifndef _LIVRE_H_
+#define _LIVRE_H_
 
 class livre : public ressource
 {
-std::string titre;
-std::string auteur;
-int annee;
-int nbr_pages;
-std::string collection;
-std::sting resume;
+private:
+    std::string titre;
+    std::string auteur;
+    int annee;
+    int nbr_pages;
+    std::string collection;
+    std::string resume;
+
+public:
+    livre(std::string titre, std::string auteur, int annee, int nbr_pages, std::string collection, std::string resume);
+    
+    void info(void);
+    void infoDetail(void);
+
 };
+
+#endif
