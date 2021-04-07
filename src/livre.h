5b0089888db85a18f3ef9bb4df747052bd8f70e0
CRESCENCE Benjamin <Benjamin.Crescence@bordeaux-inp.fr>
2021-04-07 12:16:35 +0200

    modif livre.h ressource.h

---
+++ src/livre.h
@@ -0,0 +1,12 @@
+#include "ressource.h"
+
+
+class livre : public ressource
+{
+std::string titre;
+std::string auteur;
+int annee;
+int nbr_pages;
+std::string collection;
+std::sting resume;
+};
---
+++ src/ressource.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+//#include <ctime>
+//#include <cstring>
+//#include <fstream>
+
+//using  namespace  std ;
+
+#ifndef _RESSOURCE_H_
+#define _RESSOURCE_H_
+
+
+class ressource
+{
+  enum type{LIVRE, REVUE, CD, VHS, DVD, RESNUM};
+};
+
+#endif
