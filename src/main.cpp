ada59469d334bbe8f63143bca3a6dd915812625a
Louis GUENEGO <louis.guenego@gmail.com>
2021-04-14 09:51:28 +0200

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
+#include "ressource.h"
 
 int main (int argc, char * argv [] ){
+
+	std::vector <ressource *> list;
+
+
 	std::cout << "Hello World..." << std::endl;
 	return 0;
 }
