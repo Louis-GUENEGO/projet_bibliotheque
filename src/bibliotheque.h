1be7e50046e724f719baadfe9bd7de8d1bb3f975
Louis GUENEGO <louis.guenego@gmail.com>
2021-04-14 10:03:58 +0200

    update

---
+++ src/bibliotheque.h
@@ -0,0 +1,13 @@
+#ifndef _BIBLIOTHEQUE_H_
+#define _BIBLIOTHEQUE_H_
+
+    #include "ressource.h"
+
+    #include "livre.h"
+    #include "revue.h"
+    #include "cd.h"
+    #include "vhs.h"
+    #include "dvd.h"
+    #include "resnum.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
-#include "ressource.h"
+#include "bibliotheque.h"
 
 int main (int argc, char * argv [] ){
 
 	std::vector <ressource *> list;
-
 
 	std::cout << "Hello World..." << std::endl;
 	return 0;
