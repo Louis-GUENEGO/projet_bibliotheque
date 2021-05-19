be8c5b962df4148708687e431cd300d60cf49835
Louis GUENEGO <louis.guenego@gmail.com>
2021-05-19 10:50:27 +0200

    rewiewed bib.h

--- src/bibliotheque.h
+++ src/bibliotheque.h
@@ -1,16 +1,17 @@
 #ifndef _BIBLIOTHEQUE_H_
 #define _BIBLIOTHEQUE_H_
 
-    #define DEFAULT_BIB_LOAD "load bib"
+    #define DEFAULT_BIB_LOAD "load bib" // commande de chargement automatique
 
+    // bibliothèques standards
     #include <vector>
     #include <iostream>
     #include <string>
     #include <cstdlib>
     #include <fstream>
 
+    // objet ressource et sous objets
     #include "ressource.h"
-
     #include "livre.h"
     #include "revue.h"
     #include "cd.h"
@@ -18,20 +19,21 @@
     #include "dvd.h"
     #include "resnum.h"
 
+    // interface homme machine
     void ihm (void);
 
+    // fonctions pour les commandes complexes
     void afficheBib (std::vector <ressource *> * bib);
-    void addType   (std::vector <ressource *> * bib, std::string cmd);
-    void loadBib   (std::vector <ressource *> * bib, std::string cmd);
-    void saveBib   (std::vector <ressource *> * bib, std::string cmd);
-    void resetBib  (std::vector <ressource *> * bib);
-    void searchBib (std::vector <ressource *> * bib, std::vector <ressource *> * * bibsearch, const std::string & str);
-
-    char newIdFree (std::vector <ressource *> * bib);
-
-    void showID   (std::vector <ressource *> * bib, std::string cmd);
+    void addType    (std::vector <ressource *> * bib, std::string cmd);
+    void loadBib    (std::vector <ressource *> * bib, std::string cmd);
+    void saveBib    (std::vector <ressource *> * bib, std::string cmd);
+    void resetBib   (std::vector <ressource *> * bib);
+    void searchBib  (std::vector <ressource *> * bib, std::vector <ressource *> * * bibsearch, const std::string & str);
+    char login      (void);
+    void showID     (std::vector <ressource *> * bib, std::string cmd);
     void deleteID   (std::vector <ressource *> * bib, std::string cmd);
 
-    char login (void);
+    // fonctions auxiliaires
+    char newIdFree  (std::vector <ressource *> * bib);
 
 #endif
